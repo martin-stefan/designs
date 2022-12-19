4768132150443c1db7100dc56e9ac9b943beaea3
Stéfan Martin <55285748+martin-stefan@users.noreply.github.com>
2022-12-19 13:23:54 -0500

    adding home button

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import './App.css';
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route, Link } from 'react-router-dom';
 import { items } from './core/items';
 import Directory from './core/Directory';
 
 function App() {
   return (
     <div className="App">
+
+      <Link to="/" className="homeBtn">Home</Link>
       <Routes>
         <Route path="/" element={<Directory/>}/>
         {
