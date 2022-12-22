014dc7c53a80fb8b3ff7adf769a8dbc7c0b731a8
Stéfan Martin <55285748+martin-stefan@users.noreply.github.com>
2022-12-22 17:04:37 -0500

    setting up colors and basic animation

--- src/examples/AnimatedNav/AnimatedNav.tsx
+++ src/examples/AnimatedNav/AnimatedNav.tsx
@@ -1,11 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
 import './animatedNav.css';
 
+
 const AnimatedNav = () => {
+  const bgColorsBody = ["#ffb457", "#ff96bd", "#9999fb", "#ffe797", "#cffff1"];
+  const [active, setActive] = useState(0);
+
+  const handleClick = (index:number) => {
+    setActive(index)
+  }
+
   return (
-    <div style={{display: "flex", justifyContent: "center", alignItems: "center", width: "100vw", height: "100vh"}}>
+    <div style={{display: "flex", justifyContent: "center", alignItems: "center", width: "100vw", height: "100vh", backgroundColor: bgColorsBody[active]}}>
       <div className="navbar">
-        <button className="navBtn">
+        <button className={active === 0 ? "navBtn navBtn--active" : "navBtn"} style={{backgroundColor: active === 0 ?  "#ff8c00" : ""}} onClick={() => handleClick(0)}>
           <svg className="icon" viewBox="0 0 24 24">
             <path d="M3.8,6.6h16.4"/>
             <path d="M20.2,12.1H3.8"/>
@@ -13,7 +21,7 @@ const AnimatedNav = () => {
           </svg>
         </button>
 
-        <button className="navBtn">
+        <button className={active === 1 ? "navBtn navBtn--active" : "navBtn"} style={{backgroundColor: active === 1 ?  "#f54888" : ""}} onClick={() => handleClick(1)}>
           <svg className="icon" viewBox="0 0 24 24">
             <path  d="M6.7,4.8h10.7c0.3,0,0.6,0.2,0.7,0.5l2.8,7.3c0,0.1,0,0.2,0,0.3v5.6c0,0.4-0.4,0.8-0.8,0.8H3.8
             C3.4,19.3,3,19,3,18.5v-5.6c0-0.1,0-0.2,0.1-0.3L6,5.3C6.1,5,6.4,4.8,6.7,4.8z"/>
@@ -21,7 +29,7 @@ const AnimatedNav = () => {
           </svg>
         </button>
 
-        <button className="navBtn">
+        <button className={active === 2 ? "navBtn navBtn--active" : "navBtn"} style={{backgroundColor: active === 2 ?  "#4343f5" : ""}} onClick={() => handleClick(2)}>
           <svg className="icon" viewBox="0 0 24 24">
             <path  d="M3.4,11.9l8.8,4.4l8.4-4.4"/>
             <path  d="M3.4,16.2l8.8,4.5l8.4-4.5"/>
@@ -29,7 +37,7 @@ const AnimatedNav = () => {
           </svg>
         </button>
 
-        <button className="navBtn">
+        <button className={active === 3 ? "navBtn navBtn--active" : "navBtn"} style={{backgroundColor: active === 3 ?  "#e0b115" : ""}} onClick={() => handleClick(3)}>
           <svg className="icon" viewBox="0 0 24 24" >
             <path  d="M5.1,3.9h13.9c0.6,0,1.2,0.5,1.2,1.2v13.9c0,0.6-0.5,1.2-1.2,1.2H5.1c-0.6,0-1.2-0.5-1.2-1.2V5.1
               C3.9,4.4,4.4,3.9,5.1,3.9z"/>
@@ -38,7 +46,7 @@ const AnimatedNav = () => {
           </svg>
         </button>
 
-        <button className="navBtn">
+        <button className={active === 4 ? "navBtn navBtn--active" : "navBtn"} style={{backgroundColor: active === 4 ?  "#65ddb7" : ""}} onClick={() => handleClick(4)}>
           <svg className="icon" viewBox="0 0 24 24" >
             <path  d="M5.1,3.9h13.9c0.6,0,1.2,0.5,1.2,1.2v13.9c0,0.6-0.5,1.2-1.2,1.2H5.1c-0.6,0-1.2-0.5-1.2-1.2V5.1
               C3.9,4.4,4.4,3.9,5.1,3.9z"/>
@@ -46,6 +54,16 @@ const AnimatedNav = () => {
             <path  d="M10.4,8.8c0,0.9-0.7,1.6-1.6,1.6c-0.9,0-1.6-0.7-1.6-1.6C7.3,8,8,7.3,8.9,7.3C9.7,7.3,10.4,8,10.4,8.8z"/>
           </svg>
         </button>
+      </div>
+
+      <div className="svg-container">
+        <svg viewBox="0 0 202.9 45.5">
+          <clipPath id="menu" clipPathUnits="objectBoundingBox" transform="scale(0.0049285362247413 0.021978021978022)">
+            <path  d="M6.7,45.5c5.7,0.1,14.1-0.4,23.3-4c5.7-2.3,9.9-5,18.1-10.5c10.7-7.1,11.8-9.2,20.6-14.3c5-2.9,9.2-5.2,15.2-7
+              c7.1-2.1,13.3-2.3,17.6-2.1c4.2-0.2,10.5,0.1,17.6,2.1c6.1,1.8,10.2,4.1,15.2,7c8.8,5,9.9,7.1,20.6,14.3c8.3,5.5,12.4,8.2,18.1,10.5
+              c9.2,3.6,17.6,4.2,23.3,4H6.7z"/>
+          </clipPath>
+        </svg>
       </div>
 
     </div>
